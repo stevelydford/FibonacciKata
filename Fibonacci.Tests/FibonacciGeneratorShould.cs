dff2eb495c1e473557da2fcad8e68c31c3378db4
Steve Lydford <stevelydford@gmail.com>
2016-03-03 14:58:05 +0000

    Initial commit

---
+++ Fibonacci.Tests/FibonacciGeneratorShould.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Fibonacci.Tests
+{
+    [TestFixture]
+    public class FibonacciGeneratorShould
+    {
+        private FibonacciGenerator _fibonacciGenerator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fibonacciGenerator = new FibonacciGenerator();
+        }
+
+        [TestCase(0)]
+        [TestCase(31)]
+        public void NotAcceptInvalidValues(int invalidSequenceNumber)
+        {
+            Assert.That(() => _fibonacciGenerator.Generate(invalidSequenceNumber), Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+        [TestCase(1, new[] { 0 })]
+        [TestCase(2, new[] { 0, 1 })]
+        [TestCase(3, new[] { 0, 1, 1 })]
+        [TestCase(10, new[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 })]
+        public void ReturnsTheFibonacciSequence(int sequenceNumber, int[] expectedFibonacciSequence)
+        {
+            var result = _fibonacciGenerator.Generate(sequenceNumber);
+
+            Assert.That(result, Is.EqualTo(expectedFibonacciSequence));
+        }
+    }
+}
---
+++ Fibonacci/FibonacciGenerator.cs
@@ -0,0 +1,30 @@
+﻿namespace Fibonacci
+{
+    public class FibonacciGenerator
+    {
+        public int[] Generate(int number)
+        {
+            if (number <= 0 || number > 30)
+            {
+                throw new System.ArgumentOutOfRangeException();
+            }
+
+            var fibonacciSequence = new int[number];
+
+            for (var i = 0; i < number; i++)
+            {
+                fibonacciSequence[i] = GetNumberAtPosition(i);
+            }
+
+            return fibonacciSequence;
+        }
+
+        private static int GetNumberAtPosition(int position)
+        {
+            if (position > 1)
+                return GetNumberAtPosition(position - 1) + GetNumberAtPosition(position - 2);
+
+            return position;
+        }
+    }
+}
